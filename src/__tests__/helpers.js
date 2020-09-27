6828acc5bffdcf22ee7015fb2f6bd32a56bbd71d
Varian Gilrandy R.A <itachiu603@gmail.com>
2020-09-27 13:10:21 +0700

    Merubah subtotal

--- src/__tests__/helpers.js
+++ src/__tests__/helpers.js
@@ -7,6 +7,6 @@ test ('Jika qty adalah 4, ketika dikurangi 1 jadi = 3',() =>{
     expect(helper.decrementQty(4)).toBe(3);
 });
 
-test ('Jika qty adalah 5, ketika dikurangi 1 jadi = 6',() =>{
+test ('Jika qty adalah 5, ketika ditambah 1 jadi = 6',() =>{
     expect(helper.incrementQty(5)).toBe(6);
 });
