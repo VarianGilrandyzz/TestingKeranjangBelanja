2d73b987e8f5cceea881af83dce2b3e1b254d2f4
Varian Gilrandy R.A <itachiu603@gmail.com>
2020-09-27 13:04:23 +0700

    Menambahkan testing increment

--- src/__tests__/helpers.js
+++ src/__tests__/helpers.js
@@ -2,3 +2,11 @@
     Gunakan file ini untuk membuat test case dengan Jest
 */
 const helper = require('../helpers');
+
+test ('Jika qty adalah 4, ketika dikurangi 1 jadi = 3',() =>{
+    expect(helper.decrementQty(4)).toBe(3);
+});
+
+test ('Jika qty adalah 5, ketika dikurangi 1 jadi = 6',() =>{
+    expect(helper.incrementQty(5)).toBe(6);
+});
